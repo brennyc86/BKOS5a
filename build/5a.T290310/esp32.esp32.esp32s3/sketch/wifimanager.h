86e0edc79eb16e4e3dd852b69e010098cc147c0d
brennyc86 <brennycc86@gmail.com>
2026-03-11 18:40:32 +0100

    Delete build/5a.T290310/esp32.esp32.esp32s3 directory

--- build/5a.T290310/esp32.esp32.esp32s3/sketch/wifimanager.h
+++
@@ -1,9 +0,0 @@
-#line 1 "/a0/usr/workdir/BKOS5a/wifimanager.h"
-#include <WiFiManager.h>
-#include <FS.h>
-#include <SPIFFS.h>
-#include <ArduinoJson.h> 
-
-void wifimanager();
-void wifimanager(bool forceConfig);
-void wifimanager_app(String actie);
